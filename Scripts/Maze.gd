b371e7241a8ea8f342518852d58d31db7779037d
33kingusp <33kingusp@gmail.com>
2021-10-18 00:43:55 +0900

    壁延ばし法の完成

--- Scripts/Maze.gd
+++ Scripts/Maze.gd
@@ -3,9 +3,9 @@ extends Node
 class_name Maze
 
 func _ready():
-	var maze = create_maze(5, 5)
-	print_maze(maze)
-
+	# 乱数生成
+	randomize()
+	
 
 # 現在地が床か
 func is_floor(maze:Array, x:int, y:int):
@@ -17,10 +17,6 @@ func is_wall(maze:Array, x:int, y:int):
 	return maze[x][y] == 1
 
 
-# 現在地が生成中の壁か
-func is_creating_wall(maze:Array, x:int, y:int):
-	return maze[x][y] == 2
-
 # 床をセット
 func set_floor(maze:Array, x:int, y:int):
 	maze[x][y] = 0
@@ -31,14 +27,13 @@ func set_wall(maze:Array, x:int, y:int):
 	maze[x][y] = 1
 
 
-# 生成中の壁をセット
-func set_creating_wall(maze:Array, x:int, y:int):
-	maze[x][y] = 2
-
-
+# 迷路生成
 func create_maze(size_x:int, size_y:int):
 	var maze = []
-	var startList = []
+	var start_list = []
+	
+	size_x = size_x * 2 + 1
+	size_y = size_y * 2 + 1	
 	
 	# 2次元配列を生成
 	for y_i in range(size_y):
@@ -53,22 +48,92 @@ func create_maze(size_x:int, size_y:int):
 				
 			if x_i % 2 == 0 and y_i % 2 == 0:
 				# 通路生成の開始地点リスト
-				startList.append([x_i, y_i])
+				start_list.append([x_i, y_i])
 
 	# 開始地点をシャッフル
-	startList.shuffle()
-	for cell in startList:
+	start_list.shuffle()
+	for cell in start_list:
 		if maze[cell[0]][cell[1]] == 0:
-			pass
+			var criating_wall_list = []
+			create_wall(maze, cell[0], cell[1], criating_wall_list)
 	
 	return maze
 	
+
+# 壁を延ばす
+func create_wall(maze:Array, start_x:int, start_y:int, criating_wall_list:Array):
+	# 移動可能な方向のリスト
+	var can_move_dir = []
+	var x = start_x
+	var y = start_y
+	var is_path = false
 	
+	# 上に延ばせるか
+	if is_floor(maze, start_x, start_y - 1) and not ([x, y - 2] in criating_wall_list):
+		can_move_dir.append(0)
+
+	# 右に延ばせるか
+	if is_floor(maze, start_x + 1, start_y) and not ([x + 2, y] in criating_wall_list):
+		can_move_dir.append(1)
+
+	# 下に延ばせるか
+	if is_floor(maze, start_x, start_y + 1) and not ([x, y + 2] in criating_wall_list):
+		can_move_dir.append(2)
+	
+	# 左に延ばせるか
+	if is_floor(maze, start_x - 1, start_y) and not ([x - 2, y] in criating_wall_list):
+		can_move_dir.append(3)
+	
+	if can_move_dir.size() > 0:
+		# 進める方向があるとき
+		# 現在地点を壁にする
+		set_wall(maze, x, y)
+		criating_wall_list.append([x, y])
+		
+		# 進める方向の中からランダムな方向に壁を延ばす
+		var dir = can_move_dir[randi() % can_move_dir.size()]
+		match dir:
+			0:
+				is_path = is_floor(maze, x, y - 2)
+				y -= 1
+				set_wall(maze, x, y)
+				y -= 1
+			1:
+				is_path = is_floor(maze, x + 2, y)
+				x += 1
+				set_wall(maze, x, y)
+				x += 1
+			2:
+				is_path = is_floor(maze, x, y + 2)
+				y += 1
+				set_wall(maze, x, y)
+				y += 1
+			3:
+				is_path = is_floor(maze, x - 2, y)
+				x -= 1
+				set_wall(maze, x, y)
+				x -= 1
+		
+		# 進んだ先が床だったら壁延ばしを続ける
+		if is_path:
+			create_wall(maze, x, y, criating_wall_list)
+	else:
+		# 進める方向がないとき
+		# 一歩前に戻り、引き続き壁を延ばす
+		var before = criating_wall_list.pop_back()
+		create_wall(maze, before[0], before[1], criating_wall_list)
+
+
+# マップの出力
 func print_maze(maze:Array):
-	var text = ""
 	for y in range(maze.size()):
 		for x in range(maze[y].size()):
-			text += str(maze[x][y])
-		text += "\n"
-	print(text)
+			if is_floor(maze, x, y):
+				$TileMap.set_cell(x, y, 0)
+			if is_wall(maze, x, y):
+				$TileMap.set_cell(x, y, 1)
 
+
+func _on_Button_pressed():
+	var maze = create_maze(5, 5)
+	print_maze(maze)
